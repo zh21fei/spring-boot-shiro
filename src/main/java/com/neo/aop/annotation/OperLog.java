d4d095b8108bafc9d6e3e76d8287304ac7636fde
zhangpf <zhangpf@co-think.com>
2020-10-16 17:49:11 +0800

    2.添加注释

--- src/main/java/com/neo/aop/annotation/OperLog.java
+++ src/main/java/com/neo/aop/annotation/OperLog.java
@@ -11,7 +11,7 @@ import java.lang.annotation.*;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-//
+// 注释
 public @interface OperLog {
     String value() default "";
 }
