d2cb4d5221ad00e71eaf412ff1de1990ebb9b7c3
zhangpf <zhangpf@co-think.com>
2020-10-16 18:16:18 +0800

    添加 annotation/OperLog.java

---
+++ src/main/java/com/neo/aop/annotation/OperLog.java
@@ -0,0 +1,17 @@
+package com.neo.aop.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 功能描述
+ *
+ * @author zhangpengfei
+ * @since 2020/6/16
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+
+public @interface OperLog {
+    String value() default "";
+}
